c6c5fc4cd3c29ba326e8d5058d6e86f414b02559
notTGY <microcraft47@gmail.com>
2021-09-18 16:43:34 +0300

    added my prev work

--- sem3progs/fifo-client-server/client.c
+++ sem3progs/fifo-client-server/client.c
@@ -1,6 +1,36 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 
-int main(int argc, char** argv) {
-  printf("Hello world!\n");
+#define VERSION 1
+#define MAX_STR_LEN 2048
+#define BUF_SIZE 1024
+
+int processInput(char* fname) {
+  printf("***********************************\n");
+
+  printf("%s", fname);
+
+  printf("***********************************\n");
+  return 0;
+}
+
+int main() {
+  int fd;
+  char str[MAX_STR_LEN];
+  printf("FIFO client v%d\nEnter your host:", VERSION);
+  scanf("%s", str);
+
+  if((fd = open(str, O_WRONLY)) < 0) {
+    // error
+  }
+  while(1) {
+    scanf("%s", str);
+    processInput(str);
+  }
+  
+  free(str);
   return 0;
 }
--- sem3progs/fifo-client-server/server.c
+++ sem3progs/fifo-client-server/server.c
@@ -1,6 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define DEFAULT_HOST_NAME "./fifo-server"
+#define BUF_SIZE 1024
 
 int main(int argc, char** argv) {
-  printf("Hello world!\n");
+  char fifo_name[BUF_SIZE], filename[BUF_SIZE], buf[BUF_SIZE];
+  int fd;
+  if (argc < 2) {
+    printf("fifo host not stated - defaults to '%s'", DEFAULT_HOST_NAME);
+    umask(0);
+    if(mkfifo(DEFAULT_HOST_NAME, 0600) < 0) {
+      perror("failed to make fifo");
+      exit(errno);
+    }
+    fd = open(DEFAULT_HOST_NAME, O_RDONLY);
+  } else {
+    umask(0);
+    if(mkfifo(argv[1], 0600) < 0) {
+      perror("failed to make fifo");
+      exit(errno);
+    }
+    fd = open(argv[1], O_RDONLY);
+  }
+  while(1) {
+    read(fd, buf, BUF_SIZE);
+    sscanf("%s %s", fifo_name, filename);
+  }
+
   return 0;
 }
