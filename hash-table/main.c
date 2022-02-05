b637446fff8ff0c1dd618e4424af1c77532ae4d7
notTGY <microcraft47@gmail.com>
2022-02-05 12:26:53 +0300

    memory test

--- hash-table/main.c
+++ hash-table/main.c
@@ -1,4 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include"lib/hash-table.h"
 
 #define DEBUG 1
@@ -15,5 +17,36 @@ int main() {
   }
 
   ht_destroy(my_hash_table);
+
+
+  ht* new_hash_table = ht_create();
+
+  int* values = malloc(sizeof(int) * 100);
+  for (size_t i = 1; i < 100; i++) {
+    char* key = "key_x";
+    key[4] = i;
+    values[i] = i;
+
+    ht_set(
+      new_hash_table,
+      (const char*)key,
+      (void*)&values[i]
+    );
+  }
+
+  for (size_t i = 1; i < 100; i++) {
+    char* key = "key_x";
+    key[4] = i;
+
+    int* v = (int*)ht_get(
+      new_hash_table,
+      (const char*)key
+    );
+    
+    if (v != &values[i]) {
+      if (DEBUG) printf("error at %d", (int)i);
+    }
+  }
+
 }
 
