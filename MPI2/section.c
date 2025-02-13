d855745261b4cb1643eb52c56db51d5f43000897
notTGY <microcraft47@gmail.com>
2025-02-13 11:13:41 +0300

    sections with fib numbers

--- MPI2/section.c
+++ MPI2/section.c
@@ -3,24 +3,21 @@
 
 int fib(int n) {
   if (n < 2) return 1;
-  int x = fib(n-1);
-  int y = fib(n-2);
+  unsigned long long int x, y;
+  #pragma omp parallel
+  {
+    #pragma omp sections
+    {
+      #pragma omp section
+      x = fib(n-1);
+      #pragma omp section
+      y = fib(n-2);
+    }
+  }
   return x + y;
 }
 
 int main() {
-  #pragma omp parallel
-  {
-    int rank = omp_get_thread_num();
-    int size = omp_get_num_threads();
-    #pragma omp sections
-    {
-      #pragma omp section
-      printf("rank %d; size %d\n", rank, size);
-      #pragma omp section
-      printf("rank %d; size %d\n", rank, size);
-    }
-  }
-  printf("%d\n", fib(10));
+  printf("%d\n", fib(30));
   return 0;
 }
